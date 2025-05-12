aa1998d3f861105d85cdbacf3b7393b2b3a10fcb
Md-Shajib <shajib0607@gmail.com>
2025-05-12 16:06:17 +0600

    Category Role Added

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,33 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enum/role.enume';
 
+@UseGuards(AuthGuard)
+@UseGuards(RolesGuard)
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post('add')
+  @Roles(Role.Admin, Role.Author)
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get('all')
+  @Roles(Role.Admin, Role.Author)
   findAll() {
     return this.categoryService.findAll();
   }
 
   @Get(':id/category')
+  @Roles(Role.Admin, Role.Author)
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(id);
   }
 
   @Patch(':id/update')
+  @Roles(Role.Admin, Role.Author)
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoryService.update(id, updateCategoryDto);
   }
 
   @Delete(':id/delete')
+  @Roles(Role.Admin, Role.Author)
   remove(@Param('id') id: string) {
     return this.categoryService.remove(id);
   }
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,12 +3,25 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Category])
+    TypeOrmModule.forFeature([Category]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: process.env.JWT_EXPIRES_IN}
+    }),
   ],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    CategoryService
+  ],
 })
 export class CategoryModule {}
